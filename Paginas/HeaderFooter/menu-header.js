a097691f63e2dc7eac07217ed9c5847376d36401
Felipe Faria <felipe1708001@gmail.com>
2025-11-23 16:19:50 -0300

    exclução de pastas antigas

--- Paginas/HeaderFooter/menu-header.js
+++
@@ -1,6 +0,0 @@
-  const hamburguer = document.getElementById('hamburguer');
-  const menu = document.querySelector('.ulNav');
-
-  hamburguer.addEventListener('click', () => {
-    menu.classList.toggle('active');
-  });
--- Paginas/pagina-vagas/javascript-vagas.js
+++
@@ -1,3 +0,0 @@
-function scrollToSection() {
-    document.getElementById("verMestrado").scrollIntoView({behavior: "smooth"})
-}
